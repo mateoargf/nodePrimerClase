909a06fed9005416ed7edeae049492983f827bdf
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-08-08 10:43:31 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const evenOrOdd = require('num-is-even-or-odd')
+
+console.log(evenOrOdd(25))
+console.log(evenOrOdd(22))
+console.log('este numero es de tipo', evenOrOdd(87))
+console.log('buenas noches')
+// console.log('hola mundo')
+// const saludo = (nombre) => {
+//    console.log(`hola buenos dias ${nombre}`)
+//  } 
+
+//  saludo('manuel')
